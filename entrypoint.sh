deafd6b54ad9f3636f86b37a66690942fdccfaef
Artem Zakharchenko <kettanaito@gmail.com>
2023-08-04 15:22:45 +0200

    fix: support custom binary path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,16 @@ if [ -n "${CWD}" ]; then
   cd "${CWD}"
 fi
 
+if [ -n "${ARTILLERY_BINARY_PATH}" ]; then
+  if [ ! -f "${ARTILLERY_BINARY_PATH}" ]; then
+    echo "Failed to locate Artillery binary at custom path: ${ARTILLERY_BINARY_PATH}"
+    exit 1
+  fi
+
+  ARTILLERY_BINARY="${ARTILLERY_BINARY_PATH}"
+else
+  ARTILLERY_BINARY="/home/node/artillery/bin/run"
+fi
+
 # Run the tests.
-/home/node/artillery/bin/run $1
+$ARTILLERY_BINARY $1
